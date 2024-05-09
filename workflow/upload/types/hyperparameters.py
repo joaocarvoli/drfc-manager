18f2833f731a901351fd32c75db21712d6e6a3bd
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-08 21:17:55 -0300

    refact: updating doc

--- workflow/upload/types/hyperparameters.py
+++ workflow/upload/types/hyperparameters.py
@@ -4,19 +4,22 @@ from enum import Enum
 
 class ExplorationType(Enum):
     """
-  `categorical` (for clipped_ppo training algorithm) 
-  or `additive_noise` (for sac training algorithm)
-  """
+    Exploration strategies used in training algorithms.
 
+    - `CATEGORICAL`: Used for clipped_ppo training algorithm.
+    - `ADDITIVE_NOISE`: Used for sac training algorithm.
+    """
     CATEGORICAL = 'categorical'
-    ADDITIVE_NOISE = 'addtive_noise'
+    ADDITIVE_NOISE = 'additive_noise'
 
 
 class LossType(Enum):
     """
-  Type of loss technique used to minimize the error, two available on DeepRacer: MSE (Mean Squared Error) and Huber.
-  """
+    Types of loss techniques used to minimize error.
 
+    - `MSE`: Mean Squared Error loss.
+    - `HUBER`: Huber loss.
+    """
     MSE = 'mean squared error'
     HUBER = 'huber'
 
@@ -24,10 +27,23 @@ class LossType(Enum):
 @dataclass
 class HyperParameters:
     """
-  Structure to define hyperparametes from DeepRacer with default values from documentation
-  `source: <https://github.com/aws-deepracer-community/deepracer-on-the-spot/tree/main/custom-files>`
-  """
+    Hyperparameters structure for DeepRacer with default values from documentation.
 
+    Attributes:
+    - `batch_size`: Batch size used in training (default: 64).
+    - `beta_entropy`: Beta value for entropy regularization (default: 0.01).
+    - `discount_factor`: Discount factor for future rewards (default: 0.999).
+    - `e_greedy_value`: Epsilon greedy value for exploration (default: 0.05).
+    - `epsilon_steps`: Number of steps for epsilon decay (default: 10000).
+    - `exploration_type`: Type of exploration strategy (default: `CATEGORICAL`).
+    - `loss_type`: Type of loss function used (default: `HUBER`).
+    - `lr`: Learning rate (default: 0.0003).
+    - `num_episodes_between_training`: Number of episodes between each training iteration (default: 40).
+    - `num_epochs`: Number of epochs for training (default: 3).
+    - `stack_size`: Number of frames stacked as input (default: 1).
+    - `term_cond_avg_score`: Average score threshold for terminating training (default: 100000).
+    - `term_cond_max_episodes`: Maximum number of episodes for terminating training (default: 100000).
+    """
     batch_size: int = 64
     beta_entropy: float = 0.01
     discount_factor: float = 0.999
