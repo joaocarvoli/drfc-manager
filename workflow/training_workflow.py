258dfd041daee53f8053e1f87818d229ca5cf111
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-08 21:18:47 -0300

    testing data upload

--- workflow/training_workflow.py
+++ workflow/training_workflow.py
@@ -1,9 +1,16 @@
 from upload.minio import MinioMiddleware
 from upload.types.hyperparameters import HyperParameters
+from workflow.upload.types.model_metadata import ModelMetadata
+from orjson import dumps
 
 minio = MinioMiddleware()
 
-default_hyperparameters = HyperParameters()
+hyperparameters = HyperParameters()
+model_metadata = ModelMetadata()
 
-upload_hyper_res = minio.upload_hyperparameters(hyperparameters=default_hyperparameters)
+upload_hyper_res = minio.upload_hyperparameters(hyperparameters=hyperparameters)
 print(upload_hyper_res)
+
+upload_metadata_res = minio.upload_metadata(model_metadata=model_metadata)
+
+print(upload_metadata_res)
