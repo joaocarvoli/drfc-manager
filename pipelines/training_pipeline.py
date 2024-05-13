c2185bbe285f89b76604449108a2dee1cc16915d
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-13 00:18:02 -0300

    feat: testing new transformers

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,15 +1,21 @@
-from gloe.utils import debug
+import os
 
-from pipelines.transformers.helpers import side_effect
-from pipelines.types.hyperparameters import HyperParameters
-from pipelines.types.model_metadata import ModelMetadata
-from upload.minio import MinioMiddleware
-from transformers.training import create_sagemaker_temp_files, check_if_metadata_is_available
+from pipelines.utils.docker.server import DockerClientServer
+from pipelines.transformers.general import image_has_running_container
+from pipelines.types_built.hyperparameters import HyperParameters
+from pipelines.types_built.model_metadata import ModelMetadata
+from pipelines.utils.minio import MinioMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 minio = MinioMiddleware()
 
 hyperparameters = HyperParameters()
 model_metadata = ModelMetadata()
+docker_client = DockerClientServer.get_instance()
+
+minio_tag = os.getenv('MINIO_IMAGE_REPOTAG')
 
 # upload_hyper_res = minio.upload_hyperparameters(hyperparameters=hyperparameters)
 # print(upload_hyper_res)
@@ -24,12 +30,17 @@ model_metadata = ModelMetadata()
 work_directory = '/tmp/teste'
 
 
-def training_pipeline():
-    (
-        side_effect(create_sagemaker_temp_files) >>
-        debug() >>
-        side_effect(check_if_metadata_is_available(work_directory))
-    )
-    
-    
-training_pipeline()
+# training_start_pipeline = (
+#     side_effect_no_output(create_sagemaker_temp_files)
+#     # side_effect(check_if_metadata_is_available(work_directory))
+# )
+
+# training_start_pipeline()
+
+# create_sagemaker_temp_files('/tmp/sagemaker')
+# check_if_metadata_is_available(work_directory)
+
+image_has_running_container([docker_client, minio_tag])
+
+# find_image_id_by_name([docker_client, "minio/minio:latest"])
+# check_if_image_has_container_running(tuple(docker_client, ""))
