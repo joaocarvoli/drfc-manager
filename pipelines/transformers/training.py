45820ac07c8231b5c960276edb14379ba8671aa1
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-13 17:11:47 -0300

    feat: creating transformers for upload data on minio

--- pipelines/transformers/training.py
+++ pipelines/transformers/training.py
@@ -1,6 +1,17 @@
-from gloe import transformer, partial_transformer, condition
+from io import BytesIO
+
+from gloe import transformer, partial_transformer
+from minio import Minio as MinioClient
+from minio.error import MinioException
+
 from pipelines.helpers.files_manager import create_folder, delete_files_on_folder
 from pipelines.transformers.exceptions.base import BaseExceptionTransformers
+from pipelines.types_built.hyperparameters import HyperParameters
+from pipelines.types_built.model_metadata import ModelMetadata
+from pipelines.utils.minio.utilities import upload_hyperparameters as _upload_hyperparameters
+from pipelines.utils.minio.utilities import upload_reward_function as _upload_reward_function
+from pipelines.utils.minio.utilities import upload_metadata as _upload_metadata
+
 
 sagemaker_temp_dir = '/tmp/sagemaker'
 work_directory = '/tmp/teste'
@@ -25,3 +36,34 @@ def check_if_metadata_is_available(_) -> None:
         raise BaseExceptionTransformers(exception=e)
     except Exception as e:
         raise BaseExceptionTransformers("It was not possible to check if the metadata is available", e)
+    
+    
+@partial_transformer
+def upload_hyperparameters(_, minio_client: MinioClient, hyperparameters: HyperParameters):
+    try:
+        _upload_hyperparameters(minio_client, hyperparameters)
+    except MinioException as e:
+        raise BaseExceptionTransformers(exception=e)
+    except Exception as e:
+        raise BaseExceptionTransformers("It was not possible to upload the hyperparameters", e)
+    
+    
+@partial_transformer
+def upload_metadata(_, minio_client: MinioClient, model_metadata: ModelMetadata):
+    try:
+        _upload_metadata(minio_client, model_metadata)
+    except MinioException as e:
+        raise BaseExceptionTransformers(exception=e)
+    except Exception as e:
+        raise BaseExceptionTransformers("It was not possible to upload the model metadata", e)
+
+
+@partial_transformer
+def upload_reward_function(_, minio_client: MinioClient, reward_function_buffer: BytesIO):
+    try:
+        _upload_reward_function(minio_client, reward_function_buffer)
+    except MinioException as e:
+        raise BaseExceptionTransformers(exception=e)
+    except Exception as e:
+        raise BaseExceptionTransformers("It was not possible to upload the reward function", e)
+
