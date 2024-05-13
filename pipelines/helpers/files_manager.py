aebef847f972eb4f34062fc0e9e1ca7872728a16
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-13 00:17:11 -0300

    feat: adding exceptions handling

--- pipelines/helpers/files_manager.py
+++ pipelines/helpers/files_manager.py
@@ -3,15 +3,21 @@ import os
 
 
 def create_folder(folder_name: str, mode: int = None):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name, mode=mode)
-        return True
-    return False
+    try:
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name) if mode is None else os.makedirs(folder_name, mode=mode)
+    except PermissionError:
+        raise PermissionError(f'You don\'t have permission to create folder {folder_name} with permission {mode}')
+    except Exception as e:
+        raise e
 
 
 def delete_files_on_folder(folder_name: str):
-    if os.path.exists(folder_name):
-        files = glob(f'{folder_name}/*')
-        [os.remove(file) for file in files]
-        return True
-    return False
+    try:
+        if os.path.exists(folder_name):
+            files = glob(f'{folder_name}/*')
+            [os.remove(file) for file in files]
+    except PermissionError:
+        raise PermissionError(f'You don\'t have permission to delete folder {folder_name}')
+    except Exception as e:
+        raise e
