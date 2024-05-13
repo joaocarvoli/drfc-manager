f05b2076329e9f45abfe56be5706a1e0006da722
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-13 14:18:02 -0300

    refact: improving workflow with new transformers related to docker

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,21 +1,35 @@
 import os
 
+from gloe import If
+from gloe.utils import debug
+
+from pipelines.transformers.training import create_sagemaker_temp_files, check_if_metadata_is_available
 from pipelines.utils.docker.server import DockerClientServer
-from pipelines.transformers.general import image_has_running_container
+from pipelines.transformers.general import image_tag_has_running_container, images_tags_has_some_running_container, \
+    echo, forward_condition
 from pipelines.types_built.hyperparameters import HyperParameters
 from pipelines.types_built.model_metadata import ModelMetadata
-from pipelines.utils.minio import MinioMiddleware
 from dotenv import load_dotenv
+
+from pipelines.utils.minio.minio import MinioMiddleware
 
 load_dotenv()
 
+docker_client = DockerClientServer.get_instance()
 minio = MinioMiddleware()
 
 hyperparameters = HyperParameters()
 model_metadata = ModelMetadata()
-docker_client = DockerClientServer.get_instance()
 
-minio_tag = os.getenv('MINIO_IMAGE_REPOTAG')
+
+sagemaker_tag = os.getenv('SAGEMAKER_IMAGE_REPOTAG')
+robomaker_tag = os.getenv('ROBOMAKER_IMAGE_REPOTAG')
+images_tag = []
+if sagemaker_tag is not None:
+    images_tag.append(sagemaker_tag)
+if robomaker_tag is not None:
+    images_tag.append(robomaker_tag)
+
 
 # upload_hyper_res = minio.upload_hyperparameters(hyperparameters=hyperparameters)
 # print(upload_hyper_res)
@@ -27,20 +41,18 @@ minio_tag = os.getenv('MINIO_IMAGE_REPOTAG')
 # upload_reward = minio.upload_reward_function()
 # print(upload_reward)
 
-work_directory = '/tmp/teste'
+def train_pipeline(hyperparameters: HyperParameters, model_metadata: ModelMetadata):
+    training_start_pipeline = (
+        create_sagemaker_temp_files >>
+        check_if_metadata_is_available >>
+        images_tags_has_some_running_container(docker_client, images_tag) >>
+        forward_condition
+        .Then(echo("The training is running, please stop the train before starting a new one."))
+        .Else(
+            echo("The training is not running")
+        )
+    )
+    training_start_pipeline(None)
 
 
-# training_start_pipeline = (
-#     side_effect_no_output(create_sagemaker_temp_files)
-#     # side_effect(check_if_metadata_is_available(work_directory))
-# )
-
-# training_start_pipeline()
-
-# create_sagemaker_temp_files('/tmp/sagemaker')
-# check_if_metadata_is_available(work_directory)
-
-image_has_running_container([docker_client, minio_tag])
-
-# find_image_id_by_name([docker_client, "minio/minio:latest"])
-# check_if_image_has_container_running(tuple(docker_client, ""))
+train_pipeline()
