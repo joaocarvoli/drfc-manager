8ae049d1610e795ec407b04adf227c568c6cd98a
João Victor Carvalho <joaocarvoli@hotmail.com>
2024-05-14 11:25:20 -0300

    chore: extracting dependencies from requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    DEPENDENCIES = f.readlines()
+
 setup(
     name='pipelines_drfc',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=[
-    
-    ],
+    install_requires=DEPENDENCIES
 )
